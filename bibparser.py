29b1f1690cf73a8354d608eddeb7791a6f8521d5
woyski <jonak.t.o@gmail.com>
2017-04-23 19:12:22 +0200

    halfway through

--- bibparser.py
+++ bibparser.py
@@ -6,31 +6,42 @@ import sqlite3
 
 class Record(object):
     def __init__(self, dct):
+        self.source_dict = dct
         self.ID = dct["ID"]
-        self.abstract = dct["abstract"]
+        self.abstract = self._clear_newlines(dct["abstract"])
         self.author = dct["author"]
-        self.raw_cited_references = dct["cited-references"]
-        self.doi = dct["doi"]
+        self.cited_references = self._split_citations(dct.get("cited-references", 'no_cited_references'))
+        self.doi = dct.get("doi", 'no_doi')
         self.issn = dct["issn"]
         self.journal = dct["journal"]
-        self.keyword = dct["keyword"]
+        self.keyword = dct.get("keyword", 'no_keywords')
         self.language = dct["language"]
         self.number = dct["number"]
         self.pages = dct["pages"]
         self.publisher = dct["publisher"]
         self.research_areas = dct["research-areas"]
-        self.title = dct["title"]
+        self.title = self._clear_newlines(dct["title"])
         self.type = dct["type"]
         self.unique_id = dct["unique-id"]
         self.volume = dct["volume"]
-        self.raw_web_of_science_categories = dct["web-of-science-categories"]
+        self.web_of_science_categories = self._split_categories(dct["web-of-science-categories"])
         self.year = dct["year"]
 
-    def _split_citations(self, references_str):
-        pass
+        self.is_relevant = False
 
-    def _prune_newlines(self, input_string):
-        pass
+    @property
+
+    @staticmethod
+    def _clear_newlines(abstract_str):
+        return abstract_str.replace('\n', ' ')
+
+    @staticmethod
+    def _split_citations(references_str):
+        return references_str.split(sep='\n')
+
+    @staticmethod
+    def _split_categories(categories_str):
+        return set(categories_str.split(sep='; '))
 
 
 def old_main():
@@ -45,8 +56,16 @@ def old_main():
         json.dump(bib_database.entries, json_file)
     # for record in bib_database.entries[0:1]:
 
+
 def main():
-    
+    bib_path = sys.argv[1]
+    with open(bib_path, 'r') as bibfile:
+        bibtex_str = bibfile.read()
+
+    bib_database = bibtexparser.loads(bibtex_str)
+
+    records = [Record(entry) for entry in bib_database.entries]
+    [print(record.title) for record in records]
 
 if __name__ == '__main__':
     main()
