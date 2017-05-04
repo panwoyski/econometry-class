28c6f38275bc8aa3909f8c76538db39866dabd55
woyski <jonak.t.o@gmail.com>
2017-05-04 12:03:55 +0200

    commit amounts scraping

---
+++ scrape.py
@@ -0,0 +1,56 @@
+import requests
+import sys
+import random
+import time
+
+products = [
+    'odoo',
+    'openbravo',
+    'openconcerto',
+    'postbooks',
+    'jfire',
+    'dolibarr',
+    'weberp',
+    'tryton',
+    'adempiere',
+    'erp5',
+    'Apache-OFBiz',
+    'project-open',
+    'ledger-smb',
+    'gedemin',
+    'compiere',
+    'metasfresh',
+    'mediboard',
+    'iDempiere',
+    'opentaps',
+    'tutos',
+    'erpnext',
+    'ekylibre',
+    'projecterp',
+    'web-erp',
+    'mixerp',
+    'qcadoo-mes',
+    'xendra',
+    'xerpdotnet',
+    'libertya',
+    'erpstore',
+
+]
+
+base_url = 'https://www.openhub.net/p/{}/analyses/latest/commits_history'
+
+for product in products:
+    url = base_url.format(product)
+    # print(url)
+    r = requests.get(url, stream=True, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36'})
+    if r.status_code != 200:
+        print("{}: Status code: {}, body {}".format(url, r.status_code, r.content))
+        sys.exit(1)
+
+    with open('raw/' + product + ".json", 'wb+') as f:
+        for chunk in r:
+            f.write(chunk)
+
+    print('Writing {}'.format(product))
+
+    time.sleep(random.randint(2, 5))
